49703148c1738e4e6397d254400ffd12cee9beb1
andmatcha <jj.mcr185@gmail.com>
2022-10-29 00:42:59 +0900

    refactor: :recycle: スペースを詰めた

--- src/spin_words2.ts
+++ src/spin_words2.ts
@@ -1,4 +1,4 @@
-const spinWords2 = (sentence: string) => console.log(sentence.split(" ").map(word => word.length < 6 ? word : word.split("").reverse().join("")).join(" "));
+const spinWords2=(sentence: string)=>console.log(sentence.split(" ").map(word=>word.length<6?word:word.split("").reverse().join("")).join(" "));
 
 spinWords2("Hey fellow warriors");
 spinWords2("This is a test");
